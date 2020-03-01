44a19eb277044418765056be6aa29afea6687785
sanraw <sandra.rawicz@gmail.com>
2020-03-01 14:26:41 +0100

    zad1

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = args[0];
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync(url);
+            if (result.IsSuccessStatusCode)
+            {
+                string content = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+(\\.[a-z]+)+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(content);
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+    }
+}
